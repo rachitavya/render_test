c43e8a9c0b04e6890891ac10d3ec99ce7a3cfc45
Rachitavya Sharma <rachitavya@gmail.com>
2023-11-15 14:49:58 +0530

    downloaded

--- app.py
+++ app.py
@@ -16,7 +16,9 @@ def index():
 
         # Download the PDF file
         pdf_file_path = 'downloaded_pdf.pdf'
+        print(pdf_file_path)
         download_pdf(pdf_url, pdf_file_path)
+        print('reached point 2')
 
         # Send the PDF file as an attachment for the user to download
         return send_file(pdf_file_path, as_attachment=True)
